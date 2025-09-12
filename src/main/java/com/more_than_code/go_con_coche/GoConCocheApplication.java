a784316a5fc809931e63019c71230c47af44e151
NepyAnna <nepyann@gmail.com>
2025-09-12 13:00:56 +0200

    initial commit

---
+++ src/main/java/com/more_than_code/go_con_coche/GoConCocheApplication.java
@@ -0,0 +1,13 @@
+package com.more_than_code.go_con_coche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoConCocheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoConCocheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/more_than_code/go_con_coche/GoConCocheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.more_than_code.go_con_coche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoConCocheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
