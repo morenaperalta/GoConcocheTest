6d555f33056d131aebda7e021ea0ea4e85f49726
mary kenny <marykenny2704@yahoo.com>
2025-09-16 14:17:19 +0200

    refactor(Role, RoleRepository): correct name from UserRole and UserRoleRepository

---
+++ src/main/java/com/more_than_code/go_con_coche/Role/RoleRepository.java
@@ -0,0 +1,6 @@
+package com.more_than_code.go_con_coche.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
--- src/main/java/com/more_than_code/go_con_coche/UserRole/UserRoleRepository.java
+++
@@ -1,6 +0,0 @@
-package com.more_than_code.go_con_coche.UserRole;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-}
