ac59b00ae6a9463833db0d4b3b4aa1fd56a59e95
More-Pe <peraltamorena.va@gmail.com>
2025-09-16 17:02:19 +0200

    feat(RegisteredUserRepository): add RegisteredUserRepository

---
+++ src/main/java/com/more_than_code/go_con_coche/renter_profile/RenterProfileRepository.java
@@ -0,0 +1,6 @@
+package com.more_than_code.go_con_coche.renter_profile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RenterProfileRepository extends JpaRepository<RenterProfile, Long> {
+}
