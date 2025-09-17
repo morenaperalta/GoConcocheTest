6b07f26f1f946ba0337e41a54c0749640d40ab13
NepyAnna <nepyann@gmail.com>
2025-09-17 21:21:59 +0200

    feat: add login logic to AuthService and AuthController

--- src/main/java/com/more_than_code/go_con_coche/auth/AuthController.java
+++ src/main/java/com/more_than_code/go_con_coche/auth/AuthController.java
@@ -1,9 +1,14 @@
 package com.more_than_code.go_con_coche.auth;
 
+import com.more_than_code.go_con_coche.auth.dtos.AuthRequest;
+import com.more_than_code.go_con_coche.auth.dtos.AuthResponse;
 import com.more_than_code.go_con_coche.auth.dtos.RegisterRequest;
 import com.more_than_code.go_con_coche.auth.dtos.RegisterResponse;
 import com.more_than_code.go_con_coche.auth.services.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,5 +28,14 @@ public class AuthController {
     public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest regicterDto) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(authService.register(regicterDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest loginDto,
+                                                     HttpServletResponse response) {
+        AuthResponse userResponse = authService.authenticate(loginDto, response);
+        return ResponseEntity.ok()
+                .header("Authorization", "Bearer " + userResponse.token())
+                .body(userResponse);
     }
 }
--- src/main/java/com/more_than_code/go_con_coche/auth/services/AuthService.java
+++ src/main/java/com/more_than_code/go_con_coche/auth/services/AuthService.java
@@ -2,12 +2,18 @@ package com.more_than_code.go_con_coche.auth.services;
 
 import com.more_than_code.go_con_coche.Role.Role;
 import com.more_than_code.go_con_coche.Role.RoleRepository;
+import com.more_than_code.go_con_coche.auth.dtos.AuthRequest;
+import com.more_than_code.go_con_coche.auth.dtos.AuthResponse;
 import com.more_than_code.go_con_coche.auth.dtos.RegisterRequest;
 import com.more_than_code.go_con_coche.auth.dtos.RegisterResponse;
 import com.more_than_code.go_con_coche.registered_user.RegisteredUser;
 import com.more_than_code.go_con_coche.registered_user.RegisteredUserRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -47,5 +53,31 @@ public class AuthService {
         userRepository.save(user);
 
         return new RegisterResponse(user.getId(), user.getUsername(), user.getRoles());
+    }
+
+    public AuthResponse authenticate(AuthRequest loginDto, HttpServletResponse response) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.username(), loginDto.password()));
+
+        RegisteredUser user = userRepository.findByUsername(loginDto.username())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        String jwtToken = jwtService.generateJwtToken(user.getUsername());
+        String refreshToken = jwtService.generateRefreshToken(user.getUsername());
+        Cookie refreshTokenCookie = getRefreshTokenCookie(refreshToken);
+        response.addCookie(refreshTokenCookie);
+
+        return new AuthResponse(user.getId(), user.getUsername(), jwtToken, user.getRoles());
+    }
+
+    private Cookie getRefreshTokenCookie(String token) {
+        long expirationMillis = jwtService.extractExpiration(token).getTime();
+        long remainingSeconds = (expirationMillis - System.currentTimeMillis()) / 1000;
+        int cookieMaxAge = remainingSeconds > 0 ? (int) remainingSeconds : 0;
+
+        Cookie refreshTokenCookie = new Cookie("refresh_token", token);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setPath("/");
+        refreshTokenCookie.setMaxAge(cookieMaxAge);
+
+        return refreshTokenCookie;
     }
 }
