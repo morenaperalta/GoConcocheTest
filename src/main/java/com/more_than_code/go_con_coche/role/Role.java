d6283ea911b24207ce79e400f4356bceb7735d60
NepyAnna <nepyann@gmail.com>
2025-09-17 12:01:20 +0200

    feat: add SecurityUser with implements UserDetails

--- src/main/java/com/more_than_code/go_con_coche/role/Role.java
+++ src/main/java/com/more_than_code/go_con_coche/role/Role.java
@@ -1,9 +1,9 @@
 package com.more_than_code.go_con_coche.role;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.more_than_code.go_con_coche.registered_user.RegisteredUser;
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +20,7 @@ public class Role {
     @Column (nullable = false, name = "role")
     private String role;
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "roles")
     private List<RegisteredUser> registeredUsers = new ArrayList<>();
 }
