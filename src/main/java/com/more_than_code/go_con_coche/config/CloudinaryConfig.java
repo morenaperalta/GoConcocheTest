b7d9e73d4fe69bcb449d744fd0da8d245dc98655
NepyAnna <nepyann@gmail.com>
2025-09-22 14:41:27 +0200

    feat: add CloudinaryConfig

---
+++ src/main/java/com/more_than_code/go_con_coche/config/CloudinaryConfig.java
@@ -0,0 +1,29 @@
+package com.more_than_code.go_con_coche.config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CloudinaryConfig {
+    @Value("${cloud-name}")
+    private String cloudName;
+
+    @Value("${api-key}")
+    private String apiKey;
+
+    @Value("${api-secret}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        return new Cloudinary(config);
+    }
+}
