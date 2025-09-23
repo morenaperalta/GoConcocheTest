d34fa29d8d86ff31f3444315a313c9b08f30219a
NepyAnna <nepyann@gmail.com>
2025-09-23 20:10:19 +0200

    refactor: remove user and roles ids from login and registered response

--- src/main/java/com/more_than_code/go_con_coche/auth/dtos/RegisterResponse.java
+++ src/main/java/com/more_than_code/go_con_coche/auth/dtos/RegisterResponse.java
@@ -2,7 +2,6 @@ package com.more_than_code.go_con_coche.auth.dtos;
 import com.more_than_code.go_con_coche.role.Role;
 import java.util.Set;
 
-public record RegisterResponse(Long id,
-                               String username,
-                               Set<Role> roles) {
+public record RegisterResponse(String username,
+                               Set<String> roles) {
 }
