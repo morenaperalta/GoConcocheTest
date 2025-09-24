02821cf2b6efa422a36ad9e149d562d07b6311f0
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-24 11:09:35 +0200

    feat(OwnerProfileResponse): add lombok annotation builder

--- src/main/java/com/more_than_code/go_con_coche/owner_profile/dtos/OwnerProfileResponse.java
+++ src/main/java/com/more_than_code/go_con_coche/owner_profile/dtos/OwnerProfileResponse.java
@@ -1,5 +1,8 @@
 package com.more_than_code.go_con_coche.owner_profile.dtos;
 
+import lombok.Builder;
+
+@Builder
 public record OwnerProfileResponse(
         Long id,
         Long registeredUser,
