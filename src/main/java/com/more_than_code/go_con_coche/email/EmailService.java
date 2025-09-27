0b1d6c22692d4aeda3779a91a003f4efbc05d9cc
More-Pe <peraltamorena.va@gmail.com>
2025-09-27 11:33:24 +0200

    feat(email, templates): configure email service and add welcome email template

---
+++ src/main/java/com/more_than_code/go_con_coche/email/EmailService.java
@@ -0,0 +1,36 @@
+package com.more_than_code.go_con_coche.email;
+
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+
+    public void sendRegistrationEmail(String to, String username) {
+        Context context = new Context();
+        context.setVariable("username", username);
+        String html = templateEngine.process("registration-email", context);
+        sendHtmlEmail(to, "Registration to Teach Sphere was successful.", html);
+    }
+
+    private void sendHtmlEmail(String to, String subject, String html) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(html, true);
+            mailSender.send(mimeMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
