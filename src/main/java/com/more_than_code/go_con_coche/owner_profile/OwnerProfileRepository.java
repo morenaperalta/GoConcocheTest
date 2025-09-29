2aea5000f3cb3bb345751cc4ce742809dd49c017
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-29 09:43:25 +0200

    fix(OwnerProfileRepository): delete boolean

--- src/main/java/com/more_than_code/go_con_coche/owner_profile/OwnerProfileRepository.java
+++ src/main/java/com/more_than_code/go_con_coche/owner_profile/OwnerProfileRepository.java
@@ -1,11 +1,9 @@
 package com.more_than_code.go_con_coche.owner_profile;
 
-import com.more_than_code.go_con_coche.registered_user.RegisteredUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface OwnerProfileRepository extends JpaRepository<OwnerProfile, Long> {
-    boolean findByRegisteredUserId(RegisteredUser registeredUser);
     Optional<OwnerProfile> findByRegisteredUserId(Long registeredUserId);
 }
