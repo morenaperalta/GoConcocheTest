83eeb4054530cd2a5c623ad25356a2e1b9a35cff
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-29 09:17:54 +0200

    refactor(OwnerProfileRepository): refactor boolean

--- src/main/java/com/more_than_code/go_con_coche/owner_profile/OwnerProfileRepository.java
+++ src/main/java/com/more_than_code/go_con_coche/owner_profile/OwnerProfileRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OwnerProfileRepository extends JpaRepository<OwnerProfile, Long> {
-    Optional<OwnerProfile> findByRegisteredUserId(RegisteredUser registeredUser);
+    boolean findByRegisteredUserId(RegisteredUser registeredUser);
     Optional<OwnerProfile> findByRegisteredUserId(Long registeredUserId);
 }
---
+++ src/main/java/com/more_than_code/go_con_coche/owner_profile/service/OwnerProfileServiceImpl.java
@@ -0,0 +1,4 @@
+package com.more_than_code.go_con_coche.owner_profile.service;
+
+public class OwnerProfileServiceImpl {
+}
