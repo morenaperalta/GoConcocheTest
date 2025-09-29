3b0ae52458a5d2feb8988bd46288c9d09db95115
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-09-29 09:43:25 +0200

    fix(OwnerProfileRepository): delete boolean

--- src/main/java/com/more_than_code/go_con_coche/owner_profile/OwnerProfileRepository.java
+++ src/main/java/com/more_than_code/go_con_coche/owner_profile/OwnerProfileRepository.java
@@ -1,6 +1,5 @@
 package com.more_than_code.go_con_coche.owner_profile;
 
-import com.more_than_code.go_con_coche.registered_user.RegisteredUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
