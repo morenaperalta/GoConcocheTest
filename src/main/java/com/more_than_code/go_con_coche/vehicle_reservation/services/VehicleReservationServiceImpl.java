456175cf0218c15ff42d0d28f9d870b8aab5e396
Nia <niaespinal@outlook.com>
2025-10-01 01:35:41 +0200

    feat: add slot reservation logic, generate reservation code, and send reservation confirmation email

--- src/main/java/com/more_than_code/go_con_coche/vehicle_reservation/services/VehicleReservationServiceImpl.java
+++ src/main/java/com/more_than_code/go_con_coche/vehicle_reservation/services/VehicleReservationServiceImpl.java
@@ -5,6 +5,7 @@ import com.more_than_code.go_con_coche.global.EntityNotFoundException;
 import com.more_than_code.go_con_coche.global.UnauthorizedActionException;
 import com.more_than_code.go_con_coche.renter_profile.models.RenterProfile;
 import com.more_than_code.go_con_coche.renter_profile.services.RenterProfileService;
+import com.more_than_code.go_con_coche.vehicle.dtos.VehicleOfferResponse;
 import com.more_than_code.go_con_coche.vehicle.models.Seater;
 import com.more_than_code.go_con_coche.vehicle_rental_offer.models.RentalOfferSlot;
 import com.more_than_code.go_con_coche.vehicle_rental_offer.models.VehicleRentalOffer;
@@ -15,11 +16,17 @@ import com.more_than_code.go_con_coche.vehicle_reservation.VehicleReservationRep
 import com.more_than_code.go_con_coche.vehicle_reservation.dtos.VehicleReservationMapper;
 import com.more_than_code.go_con_coche.vehicle_reservation.dtos.VehicleReservationRequest;
 import com.more_than_code.go_con_coche.vehicle_reservation.dtos.VehicleReservationResponse;
+import com.more_than_code.go_con_coche.vehicle_reservation.models.ReservationStatus;
+import com.more_than_code.go_con_coche.vehicle_reservation.models.VehicleReservation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -40,6 +47,26 @@ public class VehicleReservationServiceImpl implements VehicleReservationService{
             case SUV -> 5;
             case VAN -> 7;
         };
+    }
+
+    private void applySlotReservationStrategy(
+            RentalOfferSlot slot,
+            VehicleRentalOffer offer,
+            LocalDateTime reservationStart,
+            LocalDateTime reservationEnd) {
+
+        LocalDateTime slotStart = slot.getSlotStart();
+        LocalDateTime slotEnd = slot.getSlotEnd();
+    }
+
+    private long calculateDurationInHours (LocalDateTime start, LocalDateTime end) {
+        long minutes = ChronoUnit.MINUTES.between(start, end);
+        return (long) Math.ceil(minutes / 60.0);
+    }
+
+    private String generateReservationCode() {
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "").toUpperCase();
+        return uuid.substring(0, 3) + "-" + uuid.substring(3, 6) + "-" + uuid.substring(6, 9);
     }
 
     @Override
@@ -79,5 +106,51 @@ public class VehicleReservationServiceImpl implements VehicleReservationService{
                 }
             }
         }
+
+        for (RentalOfferSlot slot : slotsToReserve) {
+            if (slot.getSlotStart().isBefore(request.endDateTime()) && slot.getSlotEnd().isAfter(request.startDateTime())) {
+                applySlotReservationStrategy(slot, offer, request.startDateTime(), request.endDateTime());
+            }
+        }
+
+        long durationHours = calculateDurationInHours(request.startDateTime(), request.endDateTime());
+        double totalPrice = durationHours * offer.getPriceHour();
+
+        String reservationCode = generateReservationCode();
+        VehicleReservation reservation = VehicleReservation.builder()
+                .renter(renterProfile)
+                .rentalOffer(offer)
+                .reservationCode(reservationCode)
+                .startDateTime(request.startDateTime())
+                .endDateTime(request.endDateTime())
+                .travellersNumber(request.travellerNumber())
+                .totalPrice(totalPrice)
+                .status(ReservationStatus.CONFIRMED)
+                .build();
+
+        VehicleReservation savedReservation = reservationRepository.save(reservation);
+
+        VehicleOfferResponse vehicleDetails = reservationMapper.toResponse(savedReservation).vehicleDetails();
+
+        emailService.sendConfirmationEmail(
+                renterProfile.getRegisteredUser().getEmail(),
+                renterProfile.getRegisteredUser().getUsername(),
+                reservationCode,
+                request.startDateTime(),
+                request.endDateTime(),
+                vehicleDetails.brand(),
+                vehicleDetails.year(),
+                vehicleDetails.color(),
+                vehicleDetails.seats().toString(),
+                vehicleDetails.childSeatsNumber() != null ? vehicleDetails.childSeatsNumber() : 0,
+                vehicleDetails.model(),
+                (int) durationHours,
+                (int) offer.getPriceHour(),
+                (int) totalPrice,
+                offer.getLocation().getCity() + ", " + offer.getLocation().getDistrict(),
+                vehicleDetails.imageUrl()
+        );
+
+        return reservationMapper.toResponse(savedReservation);
     }
 }
