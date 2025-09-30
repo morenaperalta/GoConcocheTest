a1dd114ac92932d491534947d4c4e42a6de4c64e
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-10-01 00:06:38 +0200

    feat(OwnerProfileService): add get all owner profiles and get ownr profile by id

--- src/main/java/com/more_than_code/go_con_coche/owner_profile/service/OwnerProfileService.java
+++ src/main/java/com/more_than_code/go_con_coche/owner_profile/service/OwnerProfileService.java
@@ -4,9 +4,14 @@ import com.more_than_code.go_con_coche.owner_profile.OwnerProfile;
 import com.more_than_code.go_con_coche.owner_profile.dtos.OwnerProfileRequest;
 import com.more_than_code.go_con_coche.owner_profile.dtos.OwnerProfileResponse;
 
+import java.util.List;
+
 public interface OwnerProfileService {
     OwnerProfileResponse createOwnerProfile(OwnerProfileRequest ownerProfileRequest);
     OwnerProfileResponse getOwnerProfile();
 
     OwnerProfile getOwnerProfileObj();
+
+    List<OwnerProfileResponse> getAllOwnerProfiles();
+    OwnerProfileResponse getOwnerProfileById(Long id);
 }
