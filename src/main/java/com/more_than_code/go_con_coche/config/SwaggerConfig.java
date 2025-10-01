e2aedfe1533e5c33ed89ed3eed3aac5a6e3f08f4
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-10-01 13:10:33 +0200

    feat(all folders): add swagger config in every controller, request and swagger configuration and change security config

---
+++ src/main/java/com/more_than_code/go_con_coche/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.more_than_code.go_con_coche.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Go Con Coche API")
+                        .description("API REST Documentation for our final project")
+                        .version("v1.0"))
+                // 🔹 Añadir seguridad JWT
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
