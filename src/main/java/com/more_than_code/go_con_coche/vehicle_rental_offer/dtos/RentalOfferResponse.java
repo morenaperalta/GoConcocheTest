155e5ced396a731553ff60378c26f702380de034
Nia <niaespinal@outlook.com>
2025-10-01 16:31:21 +0200

    fix: empty location on reservation email confirmation, correct duration, price per hour. Show location info for RentalOffer and VehicleReservation

--- src/main/java/com/more_than_code/go_con_coche/vehicle_rental_offer/dtos/RentalOfferResponse.java
+++ src/main/java/com/more_than_code/go_con_coche/vehicle_rental_offer/dtos/RentalOfferResponse.java
@@ -1,5 +1,6 @@
 package com.more_than_code.go_con_coche.vehicle_rental_offer.dtos;
 
+import com.more_than_code.go_con_coche.location.dtos.LocationResponse;
 import com.more_than_code.go_con_coche.vehicle.dtos.VehicleOfferResponse;
 import lombok.Builder;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,7 @@ import java.time.LocalDateTime;
 @Builder
 public record RentalOfferResponse(Long id,
                                   VehicleOfferResponse vehicle,
-                                  Long locationId,
+                                  LocationResponse location,
                                   LocalDateTime startDateTime,
                                   LocalDateTime endDateTime,
                                   double priceHour) {
